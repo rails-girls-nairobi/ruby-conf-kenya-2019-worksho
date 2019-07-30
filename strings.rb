6dfd5664d4ce3fbf990f6909a9172754352fe968
Vishal Chandnani <vschandnani@gmail.com>
2019-07-30 09:23:12 -0400

    Add simple program for String library commands

---
+++ strings.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+actor_name = "Shah Rukh Khan"
+puts "actor name: #{actor_name}"
+puts "reversed actor name: #{actor_name.reverse}"
+puts "length of actor name: #{actor_name.length}"
